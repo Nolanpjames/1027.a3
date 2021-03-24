decf8f0e9de3fd31e9adada9741b557d55df03f1
Nolan James <56841032+Nolanpjames@users.noreply.github.com>
2021-03-24 19:06:22 -0400

    Add files via upload

---
+++ WesternCipher.java
@@ -0,0 +1,26 @@
+	import java.util.*;
+
+
+public class WesternCipher {
+
+	private char encodingQueue;
+	private char decodingQueue;
+	
+	public WesternCipher() {
+	
+	}
+	
+	public WesternCipher(String agrs[]) {
+		
+	}
+		
+	public void encode(String args[]) {
+	}
+	
+	public void decode(String args[]) {
+		
+	}
+	
+	
+		
+}
