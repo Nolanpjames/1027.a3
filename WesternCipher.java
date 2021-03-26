0ab6405a38f500dd06c5744ac0efc62cb4f1dd64
Nolan James <56841032+Nolanpjames@users.noreply.github.com>
2021-03-26 14:34:43 -0400

    Add files via upload

--- WesternCipher.java
+++ WesternCipher.java
@@ -99,10 +99,6 @@ public class WesternCipher {
 						
 						encodedChar = alphabet[alphabetPos-1];
 						
-						//when string is above 26
-						//do modulus 26, convert alphabetical position +1
-						//create 26 char array
-						//get element at position
 	
 					// Case when the previous character was not a vowel
 					} else {
@@ -127,26 +123,115 @@ public class WesternCipher {
 	}
 	
 	public String decode(String encoded) {
-		CircularArrayQueue<Integer> keyQueue1 = new CircularArrayQueue<>();
-		CircularArrayQueue<Integer> keyQueue2 = new CircularArrayQueue<>();
 		
-		for (int i = 0; i < encoded.length(); i++) {
-			keyQueue1.enqueue(new Integer(encoded.charAt(i)));
-			keyQueue2.enqueue(new Integer(encoded.charAt(i)));
+		for (int i = 0; i < input.length(); i++) {
+			this.encodingQueue.enqueue(input.charAt(i));
 		}
-		
-		Integer keyValue;
-		String decoded = "";
-		
-		for (int scan = 0; scan < encoded.length(); scan++) {
-			keyValue = keyQueue2.dequeue();
-			decoded += (char) ((int) encoded.charAt(scan) -
-					keyValue.intValue());
-			keyQueue2.enqueue(keyValue);
-		}
-		return decoded;
-	}
 
+		Character currentChar;
+		Character encodedChar = ' ';
+		String encoded = "";
+		boolean prevVowel = false;
+		Integer numEncoded = 0;
+	
+		while (!this.encodingQueue.isEmpty()) {
+			// Get the current character
+			currentChar = this.encodingQueue.dequeue();
+			
+			if (currentChar==' ') {
+				encodedChar = ' ';
+			}
+			else {
+			
+				// Case when the current character is a vowel
+				if (isVowel(currentChar)) {
+					// Case when the previous character was a vowel
+					if (prevVowel) {
+						// Convert with second table in assignment instructions
+						switch(currentChar) {
+							case '3':
+								encodedChar = 'A';
+								break;
+							case '4':
+								encodedChar = 'E';
+								break;
+							case '5':
+								encodedChar = 'I';
+								break;
+							case '6':
+								encodedChar = 'O';
+								break;
+							case '1':
+								encodedChar = 'U';
+								break;
+							case '2':
+								encodedChar = 'Y';
+								break;
+						}
+	
+					// Case when the previous character was not a vowel
+					} else {
+						// Convert with first table in assignment instructions
+						switch(currentChar) {
+							case '1':
+								encodedChar = 'A';
+								break;
+							case '2':
+								encodedChar = 'E';
+								break;
+							case '3':
+								encodedChar = 'I';
+								break;
+							case '4':
+								encodedChar = 'O';
+								break;
+							case '5':
+								encodedChar = 'U';
+								break;
+							case '6':
+								encodedChar = 'Y';
+								break;
+						}
+					}
+					// Tell the code that the previous character was a vowel
+					prevVowel = true;
+				}
+			}
+			// Case when the current character is not a vowel
+		  else {
+			char [] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+			// Case when the previous character was a vowel
+			if (prevVowel) {
+				Integer prevEncoded = numtoNum(encodedChar);
+				// Get the alphabetical position of the character to encode to
+				Integer alphabetPos = (chtoNum(currentChar) + 5 + 2*(encoded.length()) - 2*prevEncoded) % 26;
+				
+				encodedChar = alphabet[alphabetPos-1];
+				
+
+			// Case when the previous character was not a vowel
+				} else {
+				// Get the alphabetical position of the character to encode to
+					Integer alphabetPos = (chtoNum(currentChar) + 5 + 2*(encoded.length())) % 26;
+
+				// Convert the alphabetical position to the corresponding letter
+					encodedChar = alphabet[alphabetPos-1];
+				
+			}
+			// Tell the code that the previous character was not a vowel
+				prevVowel = false;
+		}
+
+	
+	// Append the encoded character to the encoded string
+		  encoded += encodedChar;
+		char prevChar = currentChar;
+
+
+		return encoded;
+		}
+	}
+					
 	// Helper function that checks whether a character is a vowel
 	private boolean isVowel(Character input) {
 		Character[] vowels = new Character[]{'A', 'E', 'I', 'O', 'U', 'Y'};
